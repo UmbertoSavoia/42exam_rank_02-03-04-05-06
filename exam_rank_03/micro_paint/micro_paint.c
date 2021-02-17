b430d7e642611f47d38cd42aa626ef918dfb8d55
Umberto Savoia <usavoia@student.42roma.it>
2021-02-17 15:50:22 +0100

    update

--- exam_rank_03/micro_paint/micro_paint.c
+++ exam_rank_03/micro_paint/micro_paint.c
@@ -46,10 +46,13 @@ int		main(int argc, char **argv)
 
 	if (argc != 2)
 		return (ft_puterror("Error: argument\n", 0));
+
 	if (!(file = fopen(argv[1], "r")))
 		return (ft_puterror("Error: Operation file corrupted\n", 0));
-	if (fscanf(file, "%d %d %c", &g_b.width, &g_b.height, &g_b.c) != 3)
+
+	if (fscanf(file, "%d %d %c\n", &g_b.width, &g_b.height, &g_b.c) != 3)
 		return (ft_puterror("Error: Operation file corrupted\n", 0));
+
 	if (!(0 < g_b.width && g_b.width <= 300 && 0 < g_b.height && g_b.height <= 300 ))
 		return (ft_puterror("Error: Operation file corrupted\n", 0));
 
@@ -59,7 +62,7 @@ int		main(int argc, char **argv)
 
 	while (check == 6)
 	{
-		if (!(g_r.width > 0.00000000 && g_r.height > 0.00000000) || !(g_r.type == 'R' || g_r.type == 'r'))
+		if (!(g_r.width > 0 && g_r.height > 0) || !(g_r.type == 'R' || g_r.type == 'r'))
 			return (ft_puterror("Error: Operation file corrupted\n", 1));
 
 		y = 0;
@@ -88,19 +91,21 @@ int		main(int argc, char **argv)
 			}
 			y++;
 		}
-
 		check = fscanf(file, "%c %f %f %f %f %c\n", &g_r.type, &g_r.start_x, &g_r.start_y, &g_r.width, &g_r.height, &g_r.color);
 	}
-	if (check != -1)
-		return (ft_puterror("Error: Operation file corrupted\n", 1));
 
-	y = 0;
-	while (y < g_b.height)
+	if (check == -1)
 	{
-		write(1, g_b.matrix + y * g_b.width, g_b.width);
-		write(1, "\n", 1);
-		y++;
+		y = 0;
+		while (y < g_b.height)
+		{
+			write(1, g_b.matrix + y * g_b.width, g_b.width);
+			write(1, "\n", 1);
+			y++;
+		}
 	}
+	else
+		return (ft_puterror("Error: Operation file corrupted\n", 1));
 
 	free(g_b.matrix);
 	fclose(file);
